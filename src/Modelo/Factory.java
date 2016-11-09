04a70fcfc823aa50cf5c12da23168e8a14c9fe1b
Blad4542 <josecar366@gmail.com>
2016-11-08 19:32:16 -0600

    ad

--- src/Modelo/Factory.java
+++ src/Modelo/Factory.java
@@ -1,20 +1,5 @@
 package Modelo;
 
-<<<<<<< HEAD
-public class Factory {
-
-    public Mesero crearMesero() {
-        return new Mesero();
-    }
-
-    public Cocinero crearCocinero(String especialidad) {
-        return new Cocinero(0, 0, especialidad);
-    }
-
-    public Cliente crearCliente() {
-        return new Cliente(0,"","","");
-    }
-=======
 public interface Factory {
     
     public Cliente crearCliente(int visitasRealizadas, String nombreCompleto, String cedula, String telefono);
@@ -23,6 +8,5 @@ public interface Factory {
     
     public Cocinero creaCocinero(int cantPlatosCocinados, int cantidadBebidasHechas, String especialidad, String direccion, String correo, String nombreCompleto, String cedula, String telefono);
        
->>>>>>> 07558e4be14aa34d5e1f6ff1082ec51eb809e7c6
 }
 
