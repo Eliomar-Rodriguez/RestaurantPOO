fe17127f15d3f712aaf7487f2519ce6b693992b8
Blad4542 <josecar366@gmail.com>
2016-11-16 14:28:07 -0600

    cambios

--- src/Empleados/Cliente.java
+++ src/Empleados/Cliente.java
@@ -31,6 +31,6 @@ public class Cliente extends Persona {
 
     @Override
     public String mostrar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "hola";
     }    
 }
